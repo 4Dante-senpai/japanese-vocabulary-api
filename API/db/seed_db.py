043063398ec16515a0dfd6311f09cc59791c5982
4Dante-senpai <demi.4d@gmail.com>
2023-06-23 17:25:29 -0300

    Add categories table

--- API/db/seed_db.py
+++ API/db/seed_db.py
@@ -3,17 +3,25 @@ from io import BytesIO
 import requests
 
 def seed(db, app):
-    sheet_id = "16lEzU_kJiVdlp77g-czverzyZ2q2i0iOmWW_fAwCeus"
-    sheets = ["foods", "verbs", "people", "numbers", "dates", "adjetives", "daily_use"]
+    sheet_id = "1_fjQaRAUUzwDe6iagQKxyK_uGeyJhvLyzpOWEO6fPAY"
+    sheet_config = "config"
+
+    url_sheets = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_config}"
+    req = requests.get(url_sheets)
+    data = req.content
+    df_categories = pd.read_csv(BytesIO(data), encoding='utf-8')
+    config_index = df_categories[df_categories['category'] == 'config'].index
+    df_categories = df_categories.drop(config_index, axis=0)
+
+    categories = df_categories.to_dict().get('category')
 
     list_df = []
-
-    for sheet in sheets:
-        url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet}"
+    for category in categories.values():
+        url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={category}"
         req = requests.get(url)
         data = req.content
         df_local = pd.read_csv(BytesIO(data), encoding='utf-8', index_col=False)
-        df_local['category'] = sheet
+        df_local['category_id'] = list(categories.keys())[list(categories.values()).index(category)]
         list_df.append(df_local)
 
     df_final = pd.concat(list_df, ignore_index=True)
@@ -24,5 +32,6 @@ def seed(db, app):
             #This create a table with "bigInt" and "text"
             #Maybe I can use the model without lose performance with pandas
             df_final.to_sql('words', engine.connect())
+            df_categories.to_sql('categories', engine.connect())
         except Exception as e:
             print(e)
