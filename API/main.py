83ccba8bb21149acad093e5499b5f05ab3532252
4Dante-senpai <demi.4d@gmail.com>
2023-07-01 22:36:24 -0300

    Fix app start before db (#1)

--- API/main.py
+++ API/main.py
@@ -17,6 +17,17 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}/{os.getenv('POSTGRES_DB')}"
     return app
 
+def create_db():
+    while True:
+        try:
+            db.init_app(app)    
+            seed_db.seed(db,app)
+            print(os.environ)
+            break
+        except Exception as e:
+            print(e)
+            print('Triying again in 20 seconds')
+
 app = create_app()
 
 api = Api(app, version='1.0', title='Japanese Vocabulary API', 
@@ -26,7 +37,5 @@ api.add_namespace(models)
 api.add_namespace(jap_ns)
 
 if __name__ == '__main__':
-    db.init_app(app)
-    seed_db.seed(db,app)
-    print(os.environ)
+    create_db()
     app.run(debug=True, host="0.0.0.0")
