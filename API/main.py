5b79519637df3ff404d62bc3eafc83f5951da82f
4Dante-senpai <demi.4d@gmail.com>
2023-07-15 08:49:25 -0300

    Fix database creation

--- API/main.py
+++ API/main.py
@@ -1,23 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from flask_restx import Api
 from db import seed_db
-from db import create_db
 from dotenv import load_dotenv
 import os
 from apis.v1.japanese import api as jap_ns
 from apis.v1.api_models import root_ns as models
 from db.db import db
+from time import sleep
 
 load_dotenv()
 
-create_db.create_japanese()
-
-def create_app():
-    app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}/{os.getenv('POSTGRES_DB')}"
-    return app
-
-def create_db():
+def create_db(app):
     while True:
         try:
             db.init_app(app)    
@@ -25,8 +18,17 @@ def create_db():
             print(os.environ)
             break
         except Exception as e:
+            sleep(20)
             print(e)
             print('Triying again in 20 seconds')
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}/{os.getenv('POSTGRES_DB')}"
+    create_db(app)
+    return app
+
+
 
 app = create_app()
 
@@ -36,6 +38,10 @@ api = Api(app, version='1.0', title='Japanese Vocabulary API',
 api.add_namespace(models)
 api.add_namespace(jap_ns)
 
+@app.route('/healthy')
+def Home():
+    response = make_response(jsonify('Healthy check'), 200)
+    return response
+
 if __name__ == '__main__':
-    create_db()
     app.run(debug=True, host="0.0.0.0")
